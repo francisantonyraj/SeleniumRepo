7e974fb0b5d900a9430bc3b56fe86d0a9b6640ae
francis <francisantontyraj1@gmail.com>
2023-01-09 19:52:57 +0530

    UpdatedFileUpload using AutoIT

--- src/test/java/com/test/scenarios/O7_HandlingFileUploadAutoIT.java
+++ src/test/java/com/test/scenarios/O7_HandlingFileUploadAutoIT.java
@@ -2,29 +2,27 @@ package com.test.scenarios;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class O7_HandlingFileUploadAutoIT {
     private WebDriver webDriver;
-    private WebElement element;
 
     @BeforeClass
     public void setUp() {
         webDriver = new ChromeDriver();
         webDriver.get("https://www.foundit.in/seeker/registration");
-        element = webDriver.findElement(By.xpath("//input[@name='file']"));
     }
 
     @Test
-    public void testCase01() {
-        element.sendKeys("C:\\Users\\Navis\\Downloads\\St.MarysGlobalEnterprices.pdf");
-    }
-
-    @Test
-    public void testCase02() {
-
+    public void testCase02() throws IOException, InterruptedException {
+        webDriver.manage().window().maximize();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath("//span[@class='browse-text']")).click();
+        Thread.sleep(3000);
+        Runtime.getRuntime().exec("C:\\Users\\Navis\\OneDrive\\Desktop\\Sample\\FileUpload.exe");
     }
 }
